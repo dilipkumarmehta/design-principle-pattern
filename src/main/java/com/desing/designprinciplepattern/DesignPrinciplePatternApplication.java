6a6b68e08ff39710d43b2e3edbc1cafaff31023a
dilipkumarmehta <dilipkumarmehta02@gmail.com>
2018-08-19 19:27:17 +0530

    initial

---
+++ src/main/java/com/desing/designprinciplepattern/DesignPrinciplePatternApplication.java
@@ -0,0 +1,12 @@
+package com.desing.designprinciplepattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPrinciplePatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPrinciplePatternApplication.class, args);
+	}
+}
